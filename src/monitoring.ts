3fd604f140472e9a49be0cd713af1f05a1fc5343
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-07-22 11:47:52 +0300

    testing

--- src/monitoring.ts
+++ src/monitoring.ts
@@ -8,22 +8,72 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { resourceFromAttributes } from '@opentelemetry/resources'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { readFileSync } from 'fs'
+import os from 'os'
 import pkgUp from 'pkg-up'
+
+const ignoredIncomingPaths = ['/health/ready', '/ping']
+
+function getRequestPath(url = '') {
+  return url.split('?')[0]
+}
+
+function getServiceName(packageJsonPath: string) {
+  const packageName = JSON.parse(readFileSync(packageJsonPath, 'utf8')).name
+
+  return (
+    process.env.OTEL_SERVICE_NAME ||
+    packageName?.replace('@', '').replace('/', '_') ||
+    'opencrvs'
+  )
+}
+
+function getResource(packageJsonPath: string) {
+  return resourceFromAttributes({
+    'service.name': getServiceName(packageJsonPath),
+    'deployment.environment':
+      process.env.OTEL_DEPLOYMENT_ENVIRONMENT ||
+      process.env.NODE_ENV ||
+      'development',
+    'host.name': os.hostname(),
+    'container.id': process.env.HOSTNAME || '',
+    'k8s.node.name': process.env.OTEL_NODE_NAME || ''
+  })
+}
+
+function initSdk(packageJsonPath: string) {
+  const sdk = new NodeSDK({
+    resource: getResource(packageJsonPath),
+    traceExporter: new OTLPTraceExporter(),
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        '@opentelemetry/instrumentation-hapi': {
+          enabled: false
+        },
+        '@opentelemetry/instrumentation-http': {
+          ignoreIncomingRequestHook: (request) =>
+            ignoredIncomingPaths.includes(getRequestPath(request.url))
+        }
+      })
+    ]
+  })
+
+  sdk.start()
+
+  process.on('SIGTERM', () => {
+    sdk.shutdown().finally(() => process.exit(0))
+  })
+}
 
 function init() {
   if (process.env.NODE_ENV === 'production') {
     const path = pkgUp.sync()
 
-    require('elastic-apm-node').start({
-      serviceName:
-        process.env.APN_SERVICE_NAME ||
-        require(path!).name.replace('@', '').replace('/', '_'),
-      serverUrl: process.env.APN_SERVICE_URL || 'http://localhost:8200',
-      containerId: process.env.HOSTNAME,
-      hostname: process.env.APN_NODE_NAME || require('os').hostname(),
-      environment:
-        process.env.APN_ENVIRONMENT || process.env.NODE_ENV || 'development'
-    })
+    initSdk(path!)
   }
 }
 init()
